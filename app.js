c86bf6213371f79b70e79db87f5a4e4a2c66bbf5
Giant09dev <aidimi2810.sgn@gmail.com>
2024-11-12 18:21:02 +0700

    remove /api

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/api/", authRoutes);
+app.use("", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
